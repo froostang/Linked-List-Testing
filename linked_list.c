36d75dff175b71602c4652b10837c2de6cc137e2
Greg <gfrantze@asu.edu>
2016-03-12 13:09:43 -0700

    ll

--- linked_list.c
+++ linked_list.c
@@ -13,7 +13,7 @@ struct node* next;
 struct linked_list{
 struct node* first;
 struct node* last;
-}
+};
 
 
 struct node* add_node(struct node* node){
@@ -25,17 +25,41 @@ scanf("%s",message);
 struct node* new_node = (struct node*)malloc(sizeof(struct node));
 
 node->next = new_node;
+
 strcpy(new_node->message, message);
 new_node->item_number = node->item_number++;
-
+new_node->next = NULL;
 
 return new_node;
 };
 
 
-void iterate_list(){};
-void print_list(){};
-void delete_and_exit(){};
+void print_list(struct node* node){
+while(node->next!=NULL){
+printf("%s\n",node->message);
+printf("List Number: %d\n",node->item_number);
+node=node->next;
+}
+
+printf("End of list\n");
+};
+
+
+
+
+void delete_and_exit(struct node* node) {
+
+struct node* temp = NULL;
+
+while(node->next!=NULL){
+temp = node;
+node=node->next;
+free(temp);
+}
+
+printf("Cleared Mem\n");
+
+};
 
 
 int main(){
@@ -52,26 +76,27 @@ head->next = NULL;
 
 
 int user_input;
-printf("What would you like to do? \n");
-printf("1: Add Item \n");
-printf("2: Print List \n");
-
-scanf("%d",&user_input);
-
 
 while(1){
+
+    printf("What would you like to do? \n");
+    printf("1: Add Item \n");
+    printf("2: Print List \n");
+
+    scanf("%d",&user_input);
 
     struct node* last_node = NULL;
 
     switch(user_input){
         case 1:
-
         last_node = add_node(linked_list->last);
         break;
 
+        case 2:
+        print_list(linked_list->first);
+        break;
 
-        case 2:print_list(); break;
-        case 3:delete_and_exit(); break;
+        case 3:delete_and_exit(linked_list->first); break;
         default: printf("error"); break;
     }
 
