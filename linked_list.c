c98c2a53e125d3a3587616f6c5f7cd193dfb5bad
Greg <gfrantze@asu.edu>
2016-03-12 12:01:50 -0700

    linked list

---
+++ linked_list.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define MAX_MESSAGE_SIZE 256
+
+struct node {
+char message [MAX_MESSAGE_SIZE];
+int item_number;
+struct node *next;
+};
+
+int main(){
+
+struct node *head = NULL;
+head = (struct *node)malloc(sizeof(struct node));
+
+
+}
